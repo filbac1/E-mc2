0c130dcefff3b73e573a33f542687ea3ce0b71e6
Filip Bačić <filipbacic08@gmail.com>
2022-05-19 18:33:39 +0200

    Commit #4

--- App.js
+++ App.js
@@ -54,19 +54,41 @@ class App extends React.Component
 
       if (view === "mapAccept")
       return (
-        <View style={{width: "100vw", height: "100vh "}}>
-            <div style="width: 100%">
-              <iframe width="100%"
-                      height="600" 
-                      frameborder="0" 
-                      scrolling="no" 
-                      marginheight="0" 
-                      marginwidth="0" 
-                      src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
-                      <a href="https://www.gps.ie/wearable-gps/">wearable trackers</a>
-              </iframe>
-            </div>
-        </View>
+        <ImageBackground source={require("./assets/img/background-mc2.jpg")} style={{width: "100%", height: "100%"}}>
+          <View style={{width: "100vw", height: "100vh", justifyContent: 'center', alignItems: 'center'}}>
+                <iframe width="50%"
+                        height="50%" 
+                        frameBorder="0" 
+                        scrolling="no" 
+                        src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+                        <a href="https://www.gps.ie/wearable-gps/">wearable trackers</a>
+                </iframe>
+          </View>
+          <View style={styles.buttonContainer4}>
+            <Button onPress={() => this.setState({ "view": "mapWalking" })} title="Accept" />
+          </View>
+          <View style={styles.buttonContainer5}>
+            <Button onPress={() => this.setState({ "view": "mapPicker" })} title="Decline" />
+          </View>
+        </ImageBackground>
+      )
+
+      if (view === "mapWalking")
+      return (
+        <ImageBackground source={require("./assets/img/background-mc2.jpg")} style={{width: "100%", height: "100%"}}>
+          <View style={{width: "100vw", height: "100vh", justifyContent: 'center', alignItems: 'center'}}>
+                <iframe width="50%"
+                        height="50%" 
+                        frameBorder="0" 
+                        scrolling="no" 
+                        src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+                        <a href="https://www.gps.ie/wearable-gps/">wearable trackers</a>
+                </iframe>
+          </View>
+          <View style={styles.circle}><Text style={styles.txt}>3.67km / 5km</Text></View>
+          <View style={styles.circle2}><Text style={styles.txt}>5 / 7 STOPS</Text></View>
+          <View style={styles.circle3}><Text style={styles.txt}>4.2km/h</Text></View>
+        </ImageBackground>
       )
 
     if (view === "login")
@@ -130,19 +152,20 @@ class App extends React.Component
           <ImageBackground source={require("./assets/img/background-mc2.jpg")} style={{width: "100%", height: "100%"}}>
             <View style={styles.container}>
               <View style={styles.checkboxContainer}>
-                <Text style={styles.label}>     Pick walking mode!</Text>
-                <Text style={styles.label}>           Free roam</Text>
-                <CheckBox style={styles.checkbox}/>
-                <Text style={styles.label}>           Dog walking</Text>
-                <CheckBox style={styles.checkbox}/>
-                <Text style={styles.label}>         Promoted content</Text>
-                <CheckBox style={styles.checkbox}/>
-                <Text style={styles.label}>           Tourist mode</Text>
-                <CheckBox style={styles.checkbox}/>
-                <Text style={styles.label}>             Jogging</Text>
-                <CheckBox style={styles.checkbox}/>
-                <Text style={styles.label}>             Custom</Text>
-                <CheckBox style={styles.checkbox}/>
+                <View style={styles.cont}>
+                  <Text style={styles.label}>          Free roam</Text>
+                  <CheckBox style={styles.checkbox}/>
+                  <Text style={styles.label}>       Dog walking</Text>
+                  <CheckBox style={styles.checkbox}/>
+                  <Text style={styles.label}>   Promoted content</Text>
+                  <CheckBox style={styles.checkbox}/>
+                  <Text style={styles.label}>        Tourist mode</Text>
+                  <CheckBox style={styles.checkbox}/>
+                  <Text style={styles.label}>           Jogging</Text>
+                  <CheckBox style={styles.checkbox}/>
+                  <Text style={styles.label}>           Custom</Text>
+                  <CheckBox style={styles.checkbox}/>
+                </View>
                 <View style={styles.submitButton2}>
                   <Button onPress={() => this.setState({ "view": "mapPicker" })} title="Submit" />
                 </View>  
@@ -167,7 +190,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     position: 'absolute',
     bottom : "20%",
-    left : "45%",
+    left : "44%",
   },
   buttonContainer3 : {
     justifyContent: 'center',
@@ -269,17 +292,80 @@ const styles = StyleSheet.create({
   },
   label: {
     margin: 8,
+    color: 'white',
+    fontWeight : 650
   },
   submitButton2 : {
     position:'absolute',
     top: "100%",
-    left : "30%"
+    left : "30%",
+    marginTop: "15%",
+    fontSize: "70px"
   },
   input: {
     height: 40,
     margin: 12,
     borderWidth: 1,
     padding: 10,
+  },
+  circle: {
+    width: 90,
+    height: 90,
+    borderRadius: 90/2,
+    backgroundColor: 'red',
+    position: 'absolute',
+    bottom: "10%",
+    left: "25%",
+    justifyContent: 'center', 
+    alignItems: 'center',
+    color: 'white'
+ },
+  circle2: {
+    width: 90,
+    height: 90,
+    borderRadius: 90/2,
+    backgroundColor: 'red',
+    position: 'absolute',
+    bottom: "10%",
+    left: "42%",
+    justifyContent: 'center', 
+    alignItems: 'center',
+    color: 'white'
+ },
+  circle3: {
+    width: 90,
+    height: 90,
+    borderRadius: 90/2,
+    backgroundColor: 'red',
+    position: 'absolute',
+    bottom: "10%",
+    left: "59%",
+    justifyContent: 'center', 
+    alignItems: 'center',
+    color: 'white'
+  },
+  txt: {
+    color: 'white',
+    fontWeight : 600
+  },
+  cont: {
+    borderWidth: 5,
+    borderColor: 'white',
+    margin: 12,
+  },
+  buttonContainer4: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top : "79%",
+    left : "45%",
+  },
+  buttonContainer5 : {
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top : "89%",
+    left : "44%",
   },
 });
 
